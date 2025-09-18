767b6b97ef97bf41341a61d584f38ff00d2235a2
SergioXDiaz <sergiodiro2000@iessanfulgencio.org>
2025-09-18 08:02:02 +0200

    modified:   miprimeraprueba - copia.php

--- miprimeraprueba - copia.php
+++ miprimeraprueba - copia.php
@@ -7,6 +7,9 @@ echo "My first PHP script!";
 ?>
 <h1> Mi primera web </h1> <!-- Sino tengo Apache levantado, no irá hace falta que funcione el servidor web
 para que te lea el PHP --> 
+
+A palo seco es un cambio  mismo 
+<!--EJEMPLO DE CAMBIO DE ARCHIVO JAJAJA-->
 </body>
 </html>
 
