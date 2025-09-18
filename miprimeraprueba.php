2011a21bc0b785a30277e5291cbd9264e336eb5d
SergioXDiaz <sergiodiro2000@iessanfulgencio.org>
2025-09-18 08:16:30 +0200

    On branch main modified:   miprimeraprueba.php

--- miprimeraprueba.php
+++ miprimeraprueba.php
@@ -2,12 +2,17 @@
 <html>
 <body>
  
-<?php
-echo "My first PHP script!";
-?>
+<?php //Inicio de código web PHP
+//ECHO SIRVE PARA MOSTRAR 
+echo "My first PHP script!"; //Las cadenas de texto van entre comillas
+//todas las sentencias deben terminar en punto y coma
+?> <!-- FIN DEL COMENTRIO EN CODIGO DE PHP -->
+
+
 <h1> Mi primera web </h1> <!-- Sino tengo Apache levantado, no irá hace falta que funcione el servidor web
 para que te lea el PHP --> 
 </body>
 </html>
 
 mODIFICACION MIA DESDE LA WEB DE GITHUB EL SERGYYYY
+PERFECTO
