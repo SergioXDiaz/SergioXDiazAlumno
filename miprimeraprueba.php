ec583e02b313a8e58b2d58a980e3763fd417e26b
SergioXDiaz <sergiodiro2000@iessanfulgencio.org>
2025-09-18 07:47:08 +0200

    new file:   miprimeraprueba - copia.php new file:   miprimeraprueba.php

---
+++ miprimeraprueba.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "My first PHP script!";
+?>
+<h1> Mi primera web </h1> <!-- Sino tengo Apache levantado, no irá hace falta que funcione el servidor web
+para que te lea el PHP --> 
+</body>
+</html>
+
