86a8e24b15c9e0c85bac3d30430dc609cef7b092
SergioXDiaz <sergiodiro2000@iessanfulgencio.org>
2025-09-22 10:43:39 +0200

    modified:   miprimeraprueba.php

--- miprimeraprueba.php
+++ miprimeraprueba.php
@@ -2,13 +2,26 @@
 <html>
 <body>
  
+
+<h1> Mi primera web </h1> <!-- Sino tengo Apache levantado, no irá hace falta que funcione el servidor web
+para que te lea el PHP --> 
+
 <?php //Inicio de código web PHP
 //ECHO SIRVE PARA MOSTRAR 
 echo "My first PHP script!"; //Las cadenas de texto van entre comillas
 //todas las sentencias deben terminar en punto y coma
 
+/*Las variables son contenedores de información
+cada vez que quieras hacer referencia a $X haremos referencia l propio dolar X
+va con el simbolo del dolar las variables
+una variable es un espacio que se reserva en memoria
 
-/* Mi primera prueba */ ?> <!-- FIN DEL COMENTRIO EN CODIGO DE PHP -->
+
+Ejercicio 3: Variables */
+$txt = "Hello World 22 de Septiembre";
+$x = 5;
+echo $x;
+?> <!-- FIN DEL COMENTRIO EN CODIGO DE PHP -->
 
 
 <h1> Mi primera web </h1> <!-- Sino tengo Apache levantado, no irá hace falta que funcione el servidor web
