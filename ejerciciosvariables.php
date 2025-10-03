e59626ca6f6e93cca8606c887816b26d3a6d6d9d
SergioXDiaz <sergiodiro2000@iessanfulgencio.org>
2025-10-03 10:25:21 +0200

    new file:   ejerciciosvariables.php

---
+++ ejerciciosvariables.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+//Ejercicios de arrays
+$Notas = array(5,3,2,7,10,9,8,2,4);
+
+
+
+//EL ARRAYS DE NOTAS CONTARÁ CON LAS NOTAS MENCIONADAS.
+//Debemos tener el número de $aprobados debe mostrar que el numero de notas aprobadas 
+// es 5
+//Mientras que el nivel de $suspensos => 4
+
+//EJERCICIO 2
+/* Variable de frutas $precio kg 10, 11.3, 1.28, 4.7, 2 
+Si el precio de una determinada fruta es mayor o igual a 4:
+    Aplico un descuento del 10 por ciento. Almaceno el valor modificado
+    en su posición.
+
+    Sino, incremento de un 5% el precio y actualizo.
+
+*/
+/*Array ejercicio1 => Calcular la media
+    -> Si la media es <5
+        $Calificacion contendrá "SUSPENSO"
+    -> Si la media es 5 o 6 -> "APROBADO"
+    -> Si la media es 7 o 8 -> "NOTABLE"
+    -> Si la media es 9 o 10 -> "SOBRESALIENTE"
+
+*/
+?>  
+
+
+
+</body>
+</html>
