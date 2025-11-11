d985404ee50acd8fae0788bedf8090713258d8b4
SergioXDiaz <sergiodiaz200027@gmail.com>
2025-11-11 01:50:30 +0100

    modified:   ejercicios_ejemplos_openwebinars.php

---
+++ ejercicios_ejemplos_openwebinars.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prácticas PHP</title>
+</head>
+<body>
+    <?php
+        echo "Hola Mundo"; //Simplemente recuerda poner punto y coma, y recuerda
+        //que echo es para mostrar por pantalla
+        /* This how works with * and / */
+        
+
+    ?>
+</body>
+</html>
